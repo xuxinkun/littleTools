ed8594a94bee2186f282bdfb3589d02fef340167
xuxinkun <xuxinkun@gmail.com>
2019-06-18 18:22:00 +0800

    edit install.sh.

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 mkdir -p /etc/littletools
 cp -r * /etc/littletools/
-sed -i '$a source /etc/littletools/*' /etc/profile
+sed -i '$a source /etc/littletools/littletools.sh' /etc/profile
 source /etc/profile
---
+++ littletools.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+source /etc/littletools/azk8spull.sh
+source /etc/littletools/docker-tools.sh
+source /etc/littletools/kube-tools.sh
